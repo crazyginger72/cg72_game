74d4d18cd1a213758e0d27037abc385d6912f14c
crazyginger72 <gingerpollard72@gmail.com>
2014-06-21 04:25:13 -0400

    Update gender.lua
    fixed and then rewriten by krock

--- mods/default/gender.lua
+++ mods/default/gender.lua
@@ -1,105 +1,115 @@
-local gender_file = minetest.get_worldpath() .. "/player_genders"
-local genders = {}
-local function loadgenders()
-local input = io.open(gender_file, "r")
-if input then
-repeat
-local gender_choice = input:read("*n")
-if gender_choice == nil then
---print("Gender choice was nil")
-break
-end
-local name = input:read("*l")
---print("playername: "..name)
-genders[name:sub(2)] = gender_choice
---print("gender: "..gender_choice)
-until input:read(0) == nil
-io.close(input)
-else
-genders = {}
-end
-end
+------------------------------------------------------------------------------
+------------------------------------------------------------------------------
+--------------boy/girl skins-- crazyginger72--rewrite by Krock----------------
+------------------------------------------------------------------------------
+-----special thanks to kaeza,Zeno`, and jordach for helping with the code-----
+------------------------------------------------------------------------------
+------------------------------------------------------------------------------
 
-loadgenders()
-
-local changed = false
-
-minetest.register_chatcommand("boy", {
-description = "Sets your player model to the default male one",
-func = function(name)
-local player = minetest.env:get_player_by_name(name)
-if player == nil then
-return false
+local gender = {}
+gender.players = {}
+ 
+gender.file = minetest.get_worldpath() .. "/player_genders"
+gender.changed = false
+ 
+gender.formname = "gender:selection"
+gender.formspec = (
+        "size[7,5]label[0,0;Are you a boy or a girl?]"..
+        "button_exit[1,1;2,1;boy;Boy]"..
+        "button_exit[3,3;2,1;girl;Girl]"
+)
+ 
+function gender.load_data()
+        local input = io.open(gender.file, "r")
+        if not input then return end
+       
+        for line in input:lines() do
+                if line ~= "" then
+                        local data = line:split(" ")
+                        gender.players[data[1]] = data[2]
+                end
+        end
+       
+        io.close(input)
 end
-if true then
-default.player_set_model(player, "character.x")
-
-player:set_properties({
-textures = {"characterm.png"},
-})
+ 
+function gender.save_data()
+        if not gender.changed then return end
+       
+        local output = io.open(gender.file, "w")
+        for k,v in pairs(gender.players) do
+                output:write(k.." "..v.."\n")
+        end
+        io.close(output)
+        gender.changed = false
 end
-player:set_local_animation({x=0, y=79}, {x=168, y=187}, {x=189, y=198}, {x=200, y=219}, 30)
-minetest.chat_send_player(name, "Set player model to male!")
-changed = true
-if changed then
-local output = io.open(gender_file, "w")
-output:write("1 "..player:get_player_name().."\n")
-io.close(output)
-end
-changed = false
-end
-
-})
-
-minetest.register_chatcommand("girl", {
-description = "Sets your player model to the female one",
-func = function(name)
-local player = minetest.env:get_player_by_name(name)
-if player == nil then
-return false
-end
-if true then
-default.player_set_model(player, "character.x")
-
-player:set_properties({
-textures = {"characterf.png"},
-})
-end
-player:set_local_animation({x=0, y=79}, {x=168, y=187}, {x=189, y=198}, {x=200, y=219}, 30)
-minetest.chat_send_player(name, "Set player model to female!")
-changed = true
-if changed then
-local output = io.open(gender_file, "w")
-output:write("0 "..player:get_player_name().."\n")
-io.close(output)
-end
-changed = false
-end
-
-})
-
-minetest.register_on_joinplayer(function(player)
-loadgenders()
-if genders[player:get_player_name()] == 1 then
-default.player_set_model(player, "character.x")
-player:set_properties({
-textures = {"characterm.png"},
-})
-player:set_local_animation({x=0, y=79}, {x=168, y=187}, {x=189, y=198}, {x=200, y=219}, 30)
-minetest.chat_send_player(player:get_player_name(), "Your gender is set to boy, to change type /girl ")
-elseif genders[player:get_player_name()] == 0 then
-default.player_set_model(player, "character.x")
-player:set_properties({
-textures = {"characterf.png"},
-})
-player:set_local_animation({x=0, y=79}, {x=168, y=187}, {x=189, y=198}, {x=200, y=219}, 30)
-minetest.chat_send_player(player:get_player_name(), "Your gender is set to girl, to change type /boy ")
-else
-default.player_set_model(player, "character.x")
-player:set_local_animation({x=0, y=79}, {x=168, y=187}, {x=189, y=198}, {x=200, y=219}, 30)
-minetest.chat_send_player(player:get_player_name(), "Please set your gender via /boy or /girl, thank you. You can also ignore this message and use the default male model. This message will be shown again when you rejoin.")
-end
+ 
+gender.load_data()
+ 
+minetest.register_on_player_receive_fields(function(player, formname, fields)
+        if gender.formname ~= formname then return end
+        local plname = player:get_player_name()
+        --default.player_set_model(player, "character.x")
+        --player:set_local_animation({x=0, y=79}, {x=168, y=187}, {x=189, y=198}, {x=200, y=219}, 30)
+       
+        if fields.boy then -- Change skin to boy.
+                player:set_properties({
+                        visual = "mesh",
+                        mesh = "character.x",
+                        textures = {"characterm.png"},
+                        visual_size = {x=1, y=1},
+                })
+                minetest.chat_send_player(plname, "Set player skin to boy!")
+               
+                gender.changed = true
+                gender.players[plname] = "m"
+        elseif fields.girl then -- Change skin to girl.
+                player:set_properties({
+                        visual = "mesh",
+                        mesh = "character.x",
+                        textures = {"characterf.png"},
+                        visual_size = {x=1, y=1},
+                })
+                minetest.chat_send_player(plname, "Set player skin to girl!")
+               
+                gender.changed = true
+                gender.players[plname] = "f"
+        end
+        gender.save_data()
 end)
-
-
+ 
+minetest.register_chatcommand("gender", {
+        description = "Set your player skin.",
+        func = function(name)
+                minetest.show_formspec(name, gender.formname, gender.formspec)
+        end
+})
+ 
+minetest.register_on_joinplayer(function(player)
+        local plname = player:get_player_name()
+        --default.player_set_model(player, "character.x")
+        --player:set_local_animation({x=0, y=79}, {x=168, y=187}, {x=189, y=198}, {x=200, y=219}, 30)
+       
+        if gender.players[plname] == "m" then
+                player:set_properties({
+                        visual = "mesh",
+                        mesh = "character.x",
+                        textures = {"characterm.png"},
+                        visual_size = {x=1, y=1},
+                })
+                minetest.chat_send_player(plname, "Your gender is set to boy, to change type /girl ")
+        elseif gender.players[plname] == "f" then
+                player:set_properties({
+                        visual = "mesh",
+                        mesh = "character.x",
+                        textures = {"characterf.png"},
+                        visual_size = {x=1, y=1},
+                })
+                minetest.chat_send_player(plname, "Your gender is set to girl, to change type /boy ")
+        else
+                minetest.chat_send_player(plname, "Please set your gender via /boy or /girl, thank you. You can also ignore this message and use the default male model. This message will be shown again when you rejoin.")
+                minetest.show_formspec(plname, gender.formname, gender.formspec)
+        end
+end)
+ 
 print("[MOD] Gender choices are loaded")
