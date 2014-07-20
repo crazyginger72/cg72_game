507d75ae29ca678fcced937aee630ff101bd43b3
crazyginger72 <gingerpollard72@gmail.com>
2014-07-20 17:46:39 -0400

    typo 2

--- mods/default/init.lua
+++ mods/default/init.lua
@@ -175,10 +175,11 @@ minetest.register_tool("default:pick_admin_with_drops", {
 --------------------------------------------------------
 
 minetest.register_on_punchnode(function(pos, node, puncher)
+local pln = player:get_player_name()
 	if puncher:get_wielded_item():get_name() == "default:pick_admin" or puncher:get_wielded_item():get_name() == "default:pick_admin_with_drops"
 	and minetest.env: get_node(pos).name ~= "air" then
 		minetest.env:remove_node(pos)
-		minetest.log("action", puncher.." uses admin-pick at "..minetest.pos_to_string(pos))
+		minetest.log("action", pln.." uses admin-pick at "..minetest.pos_to_string(pos))
 	end
 end)
 
