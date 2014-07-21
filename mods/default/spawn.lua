63d8efec975c3d7c5f16062764628529585a5373
crazyginger72 <gingerpollard72@gmail.com>
2014-07-21 00:42:58 -0400

    grs

--- mods/default/spawn.lua
+++ mods/default/spawn.lua
@@ -1,15 +1,17 @@
 minetest.register_on_chat_message(function(name, message, playername, player)
 	local cmd = "/spawn"
-	local spawn = minetest.setting_get("spawn")
+	local x = minetest.setting_get("spawn_x")
+	local y = minetest.setting_get("spawn_y")
+	local z = minetest.setting_get("spawn_z")
 	if message:sub(0, #cmd) == cmd then
 		if message == '/spawn' then
 			local player = minetest.env:get_player_by_name(name)
 			minetest.chat_send_player(player:get_player_name(), "Teleporting to spawn...")
-			player:setpos(spawn)
+			player:setpos({x y z})
 			return true
 		end
 	end
 end)
 
 
-print("/spawn is set to"..minetest.setting_get("spawn"))
+print("/spawn is set to"..x..y..x.."")
