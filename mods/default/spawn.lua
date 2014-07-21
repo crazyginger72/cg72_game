67d45d502926860c5ca3a7378ab9d2a7f34beaae
crazyginger72 <gingerpollard72@gmail.com>
2014-07-21 01:13:27 -0400

    /spawn fixed if == nil

--- mods/default/spawn.lua
+++ mods/default/spawn.lua
@@ -2,14 +2,20 @@ minetest.register_on_chat_message(function(name, message, playername, player)
 	local cmd = "/spawn"
 	local spawn = minetest.setting_get("spawn")
 	if message:sub(0, #cmd) == cmd then
-		if message == '/spawn' then
-			local player = minetest.env:get_player_by_name(name)
-			minetest.chat_send_player(player:get_player_name(), "Teleporting to spawn...")
-			player:setpos(minetest.string_to_pos(spawn))
-			return true
+
+		if swawn == nil then
+			minetest.log("action", "[ERROR] spawn not set!!!!!!!!!!!")			
+			return
+		else
+			if message == '/spawn' then
+				local player = minetest.env:get_player_by_name(name)
+				minetest.chat_send_player(player:get_player_name(), "Teleporting to spawn...")
+				player:setpos(minetest.string_to_pos(spawn))
+				return true
+			end
 		end
 	end
 end)
 
 
-print("/spawn is set to "..minetest.setting_get("spawn"))
+print("[MOD] /spawn is set to "..minetest.setting_get("spawn"))
