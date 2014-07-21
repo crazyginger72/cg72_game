2b85e3c2700ac078e8c5ee202325340e3e7ab893
crazyginger72 <gingerpollard72@gmail.com>
2014-07-21 00:35:00 -0400

    /spawn fixed

--- mods/default/spawn.lua
+++ mods/default/spawn.lua
@@ -4,8 +4,11 @@ minetest.register_on_chat_message(function(name, message, playername, player)
 		if message == '/spawn' then
 			local player = minetest.env:get_player_by_name(name)
 			minetest.chat_send_player(player:get_player_name(), "Teleporting to spawn...")
-			player:setpos({x=133, y=4, z=-82})
+			player:setpos(minetest.setting_get("static_spawnpoint"))
 			return true
 		end
 	end
 end)
+
+
+print("/spawn is set to"..minetest.setting_get("spawn"))
