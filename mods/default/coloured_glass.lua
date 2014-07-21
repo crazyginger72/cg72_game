f23d5cedef96ff00b624a7e632913d651e62637d
crazyginger72 <gingerpollard72@gmail.com>
2014-07-20 23:51:01 -0400

    new glass stuff

--- mods/default/coloured_glass.lua
+++ mods/default/coloured_glass.lua
@@ -31,6 +31,7 @@ for _, row in ipairs(coloured_glass.dyes) do
 	minetest.register_node("default:coloured_glass"..name, {
 		description = desc.." coloured glass",
 		drawtype = "glasslike",
+		paramtype = "light",
 		sunlight_propagates = true,
 		tiles = {"default_glass_"..name..".png"},
 		use_texture_alpha = true,
