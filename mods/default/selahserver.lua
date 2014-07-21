9675a596ad3fe7683be49307ab6b46a2614451e9
crazyginger72 <gingerpollard72@gmail.com>
2014-07-21 01:35:52 -0400

    new points

---
+++ mods/default/selahserver.lua
@@ -0,0 +1,67 @@
+--tps on selah's server
+
+minetest.register_on_chat_message(function(name, message, playername, player)
+	local cmd = "/porthaven"
+	local ph = minetest.setting_get("porthaven")
+	if message:sub(0, #cmd) == cmd then
+
+		if ph == nil then
+			minetest.log("action", "[ERROR] porthaven not set!!!!!!!!!!!")			
+			return
+		else
+			if message == '/porthaven' then
+				local player = minetest.env:get_player_by_name(name)
+				minetest.chat_send_player(player:get_player_name(), "Teleporting to Port Haven...")
+				player:setpos(minetest.string_to_pos(ph))
+				return true
+			end
+		end
+	end
+end)
+
+
+print("[MOD] /porthaven is set to "..minetest.setting_get("ph"))
+
+minetest.register_on_chat_message(function(name, message, playername, player)
+	local cmd = "/piratescove"
+	local pc = minetest.setting_get("piratescove")
+	if message:sub(0, #cmd) == cmd then
+
+		if pc == nil then
+			minetest.log("action", "[ERROR] piratescove not set!!!!!!!!!!!")			
+			return
+		else
+			if message == '/piratescove' then
+				local player = minetest.env:get_player_by_name(name)
+				minetest.chat_send_player(player:get_player_name(), "Teleporting to Pirates Cove...")
+				player:setpos(minetest.string_to_pos(pc))
+				return true
+			end
+		end
+	end
+end)
+
+
+print("[MOD] /admintown is set to "..minetest.setting_get("pc"))
+
+minetest.register_on_chat_message(function(name, message, playername, player)
+	local cmd = "/admintown"
+	local at = minetest.setting_get("admintown")
+	if message:sub(0, #cmd) == cmd then
+
+		if at == nil then
+			minetest.log("action", "[ERROR] admintown not set!!!!!!!!!!!")			
+			return
+		else
+			if message == '/admintown' then
+				local player = minetest.env:get_player_by_name(name)
+				minetest.chat_send_player(player:get_player_name(), "Teleporting to The Admin Town...")
+				player:setpos(minetest.string_to_pos(at))
+				return true
+			end
+		end
+	end
+end)
+
+
+print("[MOD] /admintown is set to "..minetest.setting_get("at"))
