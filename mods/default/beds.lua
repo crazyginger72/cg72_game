32703eb60f05583f5ab37d8049d8091ea7489caa
crazyginger72 <gingerpollard72@gmail.com>
2014-07-27 15:58:27 -0400

    opps forgot a line

--- mods/default/beds.lua
+++ mods/default/beds.lua
@@ -58,7 +58,6 @@ for i in ipairs(beds_list) do
 			end
 			pos2 = {x=pos.x, y=pos.y-1, z=pos.z}
 			if minetest.registered_nodes[minetest.get_node(pos).name].buildable_to  then
-			else
 				minetest.set_node(pos, node)
 			else
 				minetest.remove_node(p)
