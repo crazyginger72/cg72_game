8323563abfb577ef3603ef189f94a4b15614cc6c
crazyginger72 <gingerpollard72@gmail.com>
2014-07-27 14:28:12 -0400

    forgot to save first lol

--- mods/default/beds.lua
+++ mods/default/beds.lua
@@ -23,7 +23,7 @@ for i in ipairs(beds_list) do
 		paramtype = "light",
 		paramtype2 = "facedir",
 		stack_max = 1,
-		groups = {snappy=1,choppy=2,oddly_breakable_by_hand=2,flammable=3},
+		groups = {snappy=1,choppy=2,oddly_breakable_by_hand=2,flammable=3,bed=1},
 		sounds = default.node_sound_wood_defaults(),
 		node_box = {
 			type = "fixed",
@@ -90,7 +90,7 @@ for i in ipairs(beds_list) do
 		tiles = {"beds_bed_top_top_"..colour..".png", "default_junglewood.png",  "beds_bed_side_top_r_"..colour..".png",  "beds_bed_side_top_l_"..colour..".png",  "beds_bed_top_front.png",  "beds_bed_side_"..colour..".png"},
 		paramtype = "light",
 		paramtype2 = "facedir",
-		groups = {snappy=1,choppy=2,oddly_breakable_by_hand=2,flammable=3},
+		groups = {snappy=1,choppy=2,oddly_breakable_by_hand=2,flammable=3,bed=1},
 		sounds = default.node_sound_wood_defaults(),
 		node_box = {
 			type = "fixed",
@@ -112,6 +112,103 @@ for i in ipairs(beds_list) do
 		},
 	})
 	
+minetest.register_node("default:bed_bottom_bunk"..colour, {
+		description = beddesc,
+		drawtype = "nodebox",
+		tiles = {"beds_bed_top_bunk_bottom_"..colour..".png", "default_wood.png",  "beds_bed_side_"..colour.."_r.png",  "beds_bed_side_"..colour.."_l.png",  "beds_bed_side_"..colour..".png",  "beds_bed_side_"..colour..".png"},
+		paramtype = "light",
+		paramtype2 = "facedir",
+		groups = {snappy=1,choppy=2,oddly_breakable_by_hand=2,flammable=3,bed=1},
+		sounds = default.node_sound_wood_defaults(),
+		drop = { items = { { items = {'default:bed_bottom'..colour}, }, }, },
+		node_box = {
+			type = "fixed",
+			fixed = {
+						-- bed
+						{-0.5, -0.125, -0.5, 0.5, 0.3125, 0.5},
+						
+						-- legs
+						{-0.5, -0.5, -0.5, -0.4, 0.5, -0.4},
+						{0.4, -0.5, -0.4, 0.5, 0.5, -0.5},
+					}
+		},
+		selection_box = {
+			type = "fixed",
+			fixed = {
+						{-0.5, -0.5, -0.5, 0.5, 0.3125, 1.5},
+					}
+		},
+
+		after_place_node = function(pos, placer, itemstack)
+			local node = minetest.env:get_node(pos)
+			local p = {x=pos.x, y=pos.y, z=pos.z}
+			local param2 = node.param2
+			node.name = "default:bed_top_bunk"..colour
+			if param2 == 0 then
+				pos.z = pos.z+1
+			elseif param2 == 1 then
+				pos.x = pos.x+1
+			elseif param2 == 2 then
+				pos.z = pos.z-1
+			elseif param2 == 3 then
+				pos.x = pos.x-1
+			end
+			if minetest.registered_nodes[minetest.env:get_node(pos).name].buildable_to  then
+				minetest.env:set_node(pos, node)
+			else
+				minetest.env:remove_node(p)
+				return true
+			end
+		end,
+			
+		on_destruct = function(pos)
+			local node = minetest.env:get_node(pos)
+			local param2 = node.param2
+			if param2 == 0 then
+				pos.z = pos.z+1
+			elseif param2 == 1 then
+				pos.x = pos.x+1
+			elseif param2 == 2 then
+				pos.z = pos.z-1
+			elseif param2 == 3 then
+				pos.x = pos.x-1
+			end
+			if( minetest.env:get_node({x=pos.x, y=pos.y, z=pos.z}).name == "default:bed_top_bunk"..colour ) then
+				if( minetest.env:get_node({x=pos.x, y=pos.y, z=pos.z}).param2 == param2 ) then
+					minetest.env:remove_node(pos)
+				end	
+			end
+		end,
+	})
+	
+	minetest.register_node("default:bed_top_bunk"..colour, {
+		drawtype = "nodebox",
+		tiles = {"beds_bed_top_bunk_top_"..colour..".png", "default_junglewood.png",  "beds_bed_side_top_bunk_r_"..colour..".png",  "beds_bed_side_top_bunk_l_"..colour..".png",  "beds_bed_top_front.png",  "beds_bed_side_"..colour..".png"},
+		paramtype = "light",
+		paramtype2 = "facedir",
+		groups = {snappy=1,choppy=2,oddly_breakable_by_hand=2,flammable=3,bed=1},
+		sounds = default.node_sound_wood_defaults(),
+		{ items = { { items = {''}, }, }, },
+		node_box = {
+			type = "fixed",
+			fixed = {
+						-- bed
+						{-0.5, -0.125, -0.5, 0.5, 0.3125, 0.5},
+						{-0.4375, 0.3125, 0.1, 0.4375, 0.4375, 0.5},
+						
+						-- legs
+						{-0.4, 0.0, 0.4, -0.5, -0.5, 0.5},
+						{0.5, -0.5, 0.5, 0.4, 0.0, 0.4},
+					}
+		},
+		selection_box = {
+			type = "fixed",
+			fixed = {
+						{0, 0, 0, 0, 0, 0},
+					}
+		},
+	})
+
 	minetest.register_alias("bed_"..colour, "default:bed_bottom_"..colour)
 	
 	minetest.register_craft({
