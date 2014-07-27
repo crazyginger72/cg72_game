c5ecf58c17e22fbc07983b54557277cb3cb2d857
crazyginger72 <gingerpollard72@gmail.com>
2014-07-27 16:00:47 -0400

    forgot end

--- mods/default/beds.lua
+++ mods/default/beds.lua
@@ -276,21 +276,22 @@ minetest.register_abm({
 	interval = 1,
 	chance = 1,
 	action = function(pos, node)
-	local under = {x=pos.x, y=pos.y-1, z=pos.z}
-	local param2 = node.param2
-	if param2 == 0 then
-		topunder.z = pos.z+1
-	elseif param2 == 1 then
-		topunder.x = pos.x+1
-	elseif param2 == 2 then
-		top.z = pos.z-1
-	elseif param2 == 3 then
-		topunder.x = pos.x-1
-	end
-	local topunder = {x=topunder.x, y=topunder.y-1, z=topunder.z}
-	if minetest.get_item_group(under, "bed") == 1 then
-		minetest.swap_node(under, {name ="default:bed_bottom_bunk_"..colour})
-		minetest.swap_node(topunder, {name ="default:bed_top_bunk_"..colour})
+		local under = {x=pos.x, y=pos.y-1, z=pos.z}
+		local param2 = node.param2
+		if param2 == 0 then
+			topunder.z = pos.z+1
+		elseif param2 == 1 then
+			topunder.x = pos.x+1
+		elseif param2 == 2 then
+			top.z = pos.z-1
+		elseif param2 == 3 then
+			topunder.x = pos.x-1
+		end
+		local topunder = {x=topunder.x, y=topunder.y-1, z=topunder.z}
+		if minetest.get_item_group(under, "bed") == 1 then
+			minetest.swap_node(under, {name ="default:bed_bottom_bunk_"..colour})
+			minetest.swap_node(topunder, {name ="default:bed_top_bunk_"..colour})
+		end
 	end
 })
 
