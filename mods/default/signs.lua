c4ff262caabf28b50854060d37154daf402aa7ab
crazyginger72 <gingerpollard72@gmail.com>
2014-08-03 21:24:57 -0400

    :) tweaked hanging signs

--- mods/default/signs.lua
+++ mods/default/signs.lua
@@ -675,7 +675,7 @@ minetest.register_node(":default:sign_hanging", {
     node_box = signs_lib.hanging_sign_model.nodebox,
     selection_box = {
 		type = "fixed",
-		fixed = {-0.45, -0.275, -0.049, 0.45, 0.5, 0.049}
+		fixed = {-0.45, -0.275, -0.049, 0.45, 0, 0.049}
 	},
     tiles = {"default_junglewood.png", "default_junglewood.png", "default_junglewood.png", "default_junglewood.png", "default_junglewood.png", "default_sign_front.png"},
     groups = {choppy=2, dig_immediate=2},
