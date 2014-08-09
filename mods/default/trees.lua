42c993dee120e98478afa77e36722573576471f6
crazyginger72 <gingerpollard72@gmail.com>
2014-08-08 20:20:28 -0400

    tweak

--- mods/default/trees.lua
+++ mods/default/trees.lua
@@ -197,8 +197,8 @@ function default.grow_acaciatree(data, a, pos, seed) --watershed_acaciatree(x, y
 		elseif j == 5 +th then
 			for i = -5, 5 do
 			for k = -5, 5 do
-                        for i2 = -2, 2 do --4
-                        for k2 = -2, 2 do --4
+                        for i2 = -2, 2, 4 do
+                        for k2 = -2, 2, 4 do
                                 if math.abs(i) == 5 or math.abs(k) == 5 and math.abs(i) + math.abs(k) ~= 10 and math.abs(i) + math.abs(k) ~= 9 then 
                                         if math.random(15) ~= 2 then
                                                 local vil = a:index(xa + i, ya + j +th, za + k)
