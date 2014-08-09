3e86ab0f5a3638b0c777d7869e9e334ceebe0169
crazyginger72 <gingerpollard72@gmail.com>
2014-08-08 23:16:58 -0400

    tweak

--- mods/default/nodes.lua
+++ mods/default/nodes.lua
@@ -394,7 +394,7 @@ minetest.register_node("default:acaciatree_t_gen", {
 	tiles = {"default_acaciatree.png"},
 	paramtype2 = "facedir",
 	is_ground_content = false,
-	groups = {tree=1,choppy=2,oddly_breakable_by_hand=1,flammable=2,not_in_creative_inventory=1},
+	groups = {tree=1,choppy=2,oddly_breakable_by_hand=1,flammable=2,not_in_creative_inventory=1,treedecay=1},
 	sounds = default.node_sound_wood_defaults(),
 	on_place = minetest.rotate_node,
 	paramtype = "light",
