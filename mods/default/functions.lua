3783d5f1d017a478344c057073a83c6b814f5c6b
crazyginger72 <gingerpollard72@gmail.com>
2014-08-23 22:53:40 -0400

    tweak

--- mods/default/functions.lua
+++ mods/default/functions.lua
@@ -202,8 +202,8 @@ minetest.register_abm({
 
 minetest.register_abm({
 	nodenames = {"default:acaciasapling"},
-	interval = 1,--10,
-	chance = 1,--35,
+	interval = 10,
+	chance = 35,
 	action = function(pos, node)
 		local nu =  minetest.get_node({x=pos.x, y=pos.y-1, z=pos.z}).name
 		local is_sand = minetest.get_item_group(nu, "sand")
